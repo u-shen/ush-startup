156d757bda54ee69847b5c0e66a9e6b66594efae
u-shen <farah.ushen@gmail.com>
2025-02-24 21:32:32 -0800

    added animation by framer.motion

--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -3,11 +3,24 @@ import { Links } from "@/lib/Data";
 import Link from "next/link";
 import { useState } from "react";
 import { FaBars } from "react-icons/fa6";
+import { MdClose } from "react-icons/md";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <header className="md:container md:mx-auto relative z-[999]">
+    <motion.header
+      initial={{
+        y: -100,
+      }}
+      animate={{
+        y: 0,
+      }}
+      transition={{
+        duration: 0.5,
+      }}
+      className="bg-[#3FBBA9]/40 relative z-[999] px-10"
+    >
       <div className="header flex justify-between items-center p-4">
         <div
           style={{ fontFamily: "var(--font-cookie)" }}
@@ -16,28 +29,52 @@ export default function Header() {
           JEMMI
         </div>
         <nav className="links relative">
-          <button
+          <motion.button
+            whileTap={{
+              scaleY: 1.2,
+            }}
             className="text-3xl"
             onClick={() => setIsMenuOpen((prev) => !prev)}
           >
-            <FaBars />
-          </button>
-          {isMenuOpen && (
-            <ul className="absolute bg-slate-800/15 min-w-[250px] text-xl -left-[200px] md:-left-[200px] font-bold top-12 flex flex-col gap-4 backdrop-blur-md  p-4 rounded-md shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.]">
-              {Links.map((link) => (
-                <Link
-                  className="flex gap-2 items-center p-2"
-                  key={link.hash}
-                  href={link.hash}
-                >
-                  <div className="font-extrabold ">{link.icon}</div>
-                  {link.name}
-                </Link>
-              ))}
-            </ul>
-          )}
+            {isMenuOpen ? <MdClose /> : <FaBars />}
+          </motion.button>
+          <AnimatePresence>
+            {isMenuOpen && (
+              <motion.ul
+                initial={{
+                  height: 0,
+                  scaleY: 0,
+                }}
+                animate={{
+                  height: "auto",
+                  scaleY: 1,
+                }}
+                transition={{
+                  duration: 0.5,
+                }}
+                exit={{
+                  height: 0,
+                  scaleY: 0,
+                }}
+                className="absolute overflow-hidden bg-[#3FBBA9]/15 min-w-[250px] text-xl -left-[200px]
+                md:-left-[200px] font-bold top-[60px] flex flex-col gap-4 backdrop-blur-md
+                p-4 rounded-md"
+              >
+                {Links.map((link) => (
+                  <Link
+                    className="flex gap-2 items-center p-2"
+                    key={link.hash}
+                    href={link.hash}
+                  >
+                    <div className="font-extrabold ">{link.icon}</div>
+                    {link.name}
+                  </Link>
+                ))}
+              </motion.ul>
+            )}
+          </AnimatePresence>
         </nav>
       </div>
-    </header>
+    </motion.header>
   );
 }
--- src/components/Landing/page.tsx
+++ src/components/Landing/page.tsx
@@ -1,9 +1,40 @@
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
 export default function Landing() {
   return (
-    <div className="home">
-      <h1 className="text-[5rem] my-20 flex justify-center text-center ">
-        Duis aute irure dolor <br /> in Dolor
-      </h1>
-    </div>
+    <motion.section
+      whileInView={{
+        y: [0, 100],
+      }}
+      className="flex max-md:flex-col-reverse min-h-[50vh] justify-center container mx-auto  my-20"
+    >
+      <div className="main-landing flex-shrink-0 flex flex-col gap-8">
+        <small className="">Free 30 Days TRIAL</small>
+        <h1 className="text-6xl font-bold">
+          The Best way <br />
+          to Showcase <br />
+          Your Projects
+        </h1>
+        <p>Here You Can Put a Short Descroption about your projects</p>
+        <div className="button flex gap-3">
+          <button className="bg-[#3FB8A9] px-4 py-2 rounded-md">
+            Contatcs Us
+          </button>
+          <button className="px-4 py-2 rounded-md border border-[#3FB8A9] ">
+            Show Our Projects
+          </button>
+        </div>
+      </div>
+      <div className="image  relative max-w-full">
+        <Image
+          width={750}
+          height={390}
+          className="object-contain"
+          src="/banner-art.svg"
+          alt="Banner"
+        />
+      </div>
+    </motion.section>
   );
 }
